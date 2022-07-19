be4bb46ecf91ba7b0c3cbbb915e24f0b5d7dc270
LoomZero <p.sagitza@loom.de>
2022-07-19 11:18:01 +0200

    local update

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 RED='\033[0;31m';
 RESET='\033[0m';
 
+git add -A
+git commit -m"local update"
+
 git fetch
 git merge
 
@@ -18,14 +21,22 @@ if [[ $? != 0 ]]; then
     if [[ $input == 'e' ]]; then
       exit;
     fi;
-    check=$(git ls-files -u)
-    if [[ -n $check ]]; then
-      echo "${RED}There are still merge conflicts, please resolve, add and commit! Then press 'c'.${RESET}";
-      input="g";
+    if [[ $input == 'c' ]]; then
+      git merge-base --is-ancestor master origin/master
+      if [[ $? == 0 ]]; then
+        echo '0'
+      else
+        echo '1'
+      fi
+      check=$(git ls-files -u)
+      if [[ -n $check ]]; then
+        echo "${RED}There are still merge conflicts, please resolve, add and commit! Then press 'c'.${RESET}";
+        input="g";
+      fi
     fi
   done
 fi
-
+exit;
 node ./index.js
 git add -A
 git commit -m"update"
